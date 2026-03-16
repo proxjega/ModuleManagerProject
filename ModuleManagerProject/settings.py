cb5a77b71353876ae5934099017579bc9c6b016d
Nikita Jegorov <proxjega@gmail.com>
2026-03-16 17:43:32 +0200

    future readme, few views

--- ModuleManagerProject/settings.py
+++ ModuleManagerProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ModuleManagerApp.apps.ModulemanagerappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
