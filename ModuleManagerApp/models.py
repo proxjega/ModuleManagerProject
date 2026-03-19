3d8b37688e2fd8d0313e910984b69b676c0d3f37
Nikita Jegorov <proxjega@gmail.com>
2026-03-19 15:06:47 +0200

    profile model

--- ModuleManagerApp/models.py
+++ ModuleManagerApp/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
 
+class Profile(models.Model):
+    DEGREE_BACHELOR = "bachelor"
+    DEGREE_MASTER = "master"
+    DEGREE_DOCTORAL = "doctoral"
+    
+    DEGREE_CHOICES = [
+    (DEGREE_BACHELOR, "Bachelor"),
+    (DEGREE_MASTER, "Master"),
+    (DEGREE_DOCTORAL, "Doctoral"),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    study_institution = models.CharField(max_length=50)
+    degree = models.CharField(max_length=8, choices=DEGREE_CHOICES, default=DEGREE_BACHELOR)
+    name_of_program = models.CharField(max_length=50)
+    start_year = models.IntegerField(validators=[
+        MinValueValidator(1970),
+        MaxValueValidator(timezone.now().year + 1),
+    ])
+    
 
 class Module(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="modules")
