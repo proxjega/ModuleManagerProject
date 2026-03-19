32bd7af5271d06818a4ec8f0a6cb19f3a3bb1af6
Nikita Jegorov <proxjega@gmail.com>
2026-03-19 14:35:07 +0200

    register user almost working

--- ModuleManagerApp/services.py
+++ ModuleManagerApp/services.py
@@ -9,4 +9,16 @@ def create_module(user, data):
         teacher=data["teacher"],
         description=data.get("description", "")
     )
+    module.save()
     return module
+
+def register_user(data):
+    user = User.objects.create(
+        username=data["username"],
+        password=data["password1"],
+        email=data["email"],
+        first_name=data["first_name"],
+        last_name=data["last_name"]
+    )
+    user.save()
+    return user
--- ModuleManagerApp/views.py
+++ ModuleManagerApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 from . import forms
 from . import services
@@ -8,7 +9,13 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def register(request):
-    pass
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            services.register_user(form.cleaned_data)
+            return redirect("successful-registration") #do this
+    return render(request, "registration/register.html", {"form":form}) # template
 
 def module_create(request):
     if request.method == "POST":
